83d9d2ca229871a17137064e547a2cba1ab9277b
Molokov Klim <ultrakawaii9654449192@gmail.com>
2025-09-25 20:13:32 +0300

    0.0.0

--- builder.py
+++ builder.py
@@ -1,30 +1,24 @@
 from typing import Any
 
-from shadowstep.page_object.page_object_generator import PageObjectGenerator
-from shadowstep.page_object.page_object_parser import PageObjectParser
 from shadowstep.page_object.page_object_recycler_explorer import PageObjectRecyclerExplorer
 from shadowstep.shadowstep import Shadowstep
 
 app = Shadowstep()
-
 capabilities: dict[str, Any] = {
     "platformName": "android",
     "appium:automationName": "uiautomator2",
-    "appium:UDID": '192.168.30.101:5555',
+    "appium:UDID": '127.0.0.1:6555',
     "appium:noReset": True,
     "appium:autoGrantPermissions": True,
     "appium:newCommandTimeout": 900,
 }
-
 app.connect(capabilities=capabilities,
             server_ip='127.0.0.1',
             server_port=4723)
 
 try:
-
-    page_object_explorer = PageObjectRecyclerExplorer(app)
+    page_object_explorer = PageObjectRecyclerExplorer(app, translator=None)
     page_object_explorer.explore("pages")
-
 except Exception as error:
     print(error)
 finally:
