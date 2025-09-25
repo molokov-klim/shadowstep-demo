e610662f1cb01b94daed3f12c10362e16181ee22
Molokov Klim <ultrakawaii9654449192@gmail.com>
2025-09-25 17:31:17 +0300

    0.0.0

--- builder.py
+++ builder.py
@@ -4,7 +4,6 @@ from shadowstep.page_object.page_object_generator import PageObjectGenerator
 from shadowstep.page_object.page_object_parser import PageObjectParser
 from shadowstep.page_object.page_object_recycler_explorer import PageObjectRecyclerExplorer
 from shadowstep.shadowstep import Shadowstep
-from shadowstep.utils.translator import YandexTranslate
 
 app = Shadowstep()
 
@@ -22,21 +21,9 @@ app.connect(capabilities=capabilities,
             server_port=4723)
 
 try:
-    parser = PageObjectParser()
-    translator = YandexTranslate(folder_id="b1ghf7n3imfg7foodstv")
-    generator = PageObjectGenerator(translator)
-    recycler_explorer = PageObjectRecyclerExplorer(app, translator)
 
-    # self.shadowstep.driver.update_settings(settings={'enableMultiWindows': True})
-    # time.sleep(10)
-
-    source = app.driver.page_source
-    print(source)
-    tree = parser.parse(source)
-    # generator.generate(ui_element_tree=tree, output_dir="pages")
-    recycler_explorer.explore('pages')
-
-    # self.shadowstep.driver.update_settings(settings={'enableMultiWindows': False})
+    generator = PageObjectGenerator()
+    recycler_explorer = PageObjectRecyclerExplorer(app)
 
 
 
@@ -48,6 +35,6 @@ finally:
         app.disconnect()
     except Exception as e:
         if 'NoSuchSessionException' in str(e):
-            print("Сессия уже завершена.")
+            print("Disconnected.")
         else:
             raise
